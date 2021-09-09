61beb5fa9e50c9150cd7a9032254f3e76fade65e
Jon <jbrundagejr@gmail.com>
2021-09-09 13:23:17 -0400

    decrement tests started

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import {useState} from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [error, setError] = useState("")
 
   function addCount(){
     setCount(count + 1)
+    if(count === 0){
+      setError(false)
+    }
   }
 
   function minusCount(){
     setCount(count - 1)
+    if(count <= 0){
+      setCount(0)
+      setError(true)
+    }
   }
 
   return (
-    <div className="App" data-test="">
-      <p>The count is {count}</p>
-      <button onClick={addCount}>Plus One</button>
-      <button onClick={minusCount}>Minus One</button>
+    <div data-test="component-app">
+      <p data-test="counter-display">The count is <span data-test='display-number'>{count}</span></p>
+      {error ? <p>Counter can't go below 0</p> : null}
+      <button data-test="plus-one-button" onClick={addCount}>Plus One</button>
+      <button data-test="minus-one-button" onClick={minusCount}>Minus One</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,23 +5,49 @@ import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter() })
 
+// create the ShallowWrapper for the App component
+const setup = () => shallow(<App />)
+
+const findByTestAttribute = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.exists()).toBe(true)
+  const wrapper = setup()
+  const appComponent = findByTestAttribute(wrapper, "component-app")
+  expect(appComponent.length).toBe(1)
 });
 
 test('renders plus button', () => {
-
+  const wrapper = setup()
+  const button = findByTestAttribute(wrapper, "plus-one-button")
+  expect(button.length).toBe(1)
 })
 
 test('renders counter display', () => {
-
+  const wrapper = setup()
+  const counterDisplay = findByTestAttribute(wrapper, "counter-display")
+  expect(counterDisplay.length).toBe(1)
 })
 
 test('counter display starts at 0', () => {
-
+  const wrapper = setup()
+  const displayNumber = findByTestAttribute(wrapper, "display-number").text()
+  expect(displayNumber).toBe("0")
 })
 
 test('clicking button adds one to counter display', () => {
+  const wrapper = setup()
+  const plusOneButton = findByTestAttribute(wrapper, "plus-one-button")
+  plusOneButton.simulate('click')
+  const displayNumber = findByTestAttribute(wrapper, "display-number").text()
+  expect(displayNumber).toBe("1")
+})
 
+test('clicking minus one button decrements counter display when greater than 0', () => {
+  const wrapper = setup()
+  const plusOneButton = findByTestAttribute(wrapper, "plus-one-button")
+  plusOneButton.simulate('click')
+  const minusOneButton = findByTestAttribute(wrapper, "minus-one-button")
+  minusOneButton.simulate('click')
+  const displayNumber = findByTestAttribute(wrapper, "display-number").text()
+  expect(displayNumber).toBe("0")
 })
